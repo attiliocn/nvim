a07cf5c945f11bd276b22e0f3792fd9819f1a173
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-06-09 13:03:52 -0300

    initial commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,17 @@
+vim.cmd('set autoindent')
+vim.cmd('set expandtab')
+vim.cmd('set tabstop=4')
+vim.cmd('set shiftwidth=4')
+vim.cmd('set shiftround')
+vim.cmd('set number')
+
+vim.cmd('set backspace=2')
+vim.cmd('set showcmd')
+vim.cmd('set laststatus=2')
+vim.cmd('set autowrite')
+vim.cmd('set cursorline')
+vim.cmd('set autoread')
+
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
